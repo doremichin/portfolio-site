90070bac03a41071bdf8a7725c64ce0932b32363
doremichin <johnkim994@naver.com>
2021-12-23 02:16:42 +0900

    Revert "Revert "[Url] Image url 변경""
    This reverts commit 89f6dbb2c0c5d5235c65e30d13a220b554dcbf84.

--- src/views/main/components/about/About.js
+++ src/views/main/components/about/About.js
@@ -14,7 +14,7 @@ export default function About() {
         <SectionTitle title={appData.about.title} />
         <Wrapper>
           <ImageWrapper>
-            <img src="https://drive.google.com/uc?id=1AA8WO9mWeZJlk9EZMlgeODQ_FuHI_C0p" alt="" />
+            <img src="https://lh3.googleusercontent.com/7TskIRpGBM_vy3lIwekMVSGKXD7qv_lri3cI-_2E1_6RrDNHrjVzt7Wdp7hdZKKUSlQx2VF-obJCiXw-blLk9XHGtmfnqjy7Oil2R9TsK7gocOgXiXwqDoVO2uZjNXetsH_CsMRS2r2RrFvmwaGZxcVW6_yVFZ7EpskWNXMstLa7Cqrfn6itjdGNP9iU_5RMpMPBZ7oeERS4r3rj7F2TznJNkVgpiEZYTICARaMcO0mS8iX6nWD1iQEJScemT07n43nQtso1OywEvx_ZnKfetafezA7AkpO8MkF-kL4a2CBFBbw1suS7gpGzmNrTCuVwO-lmm7Fk-sfaNR-4kSK1XQtLqJbuy3eoT1NSwyXp42Ps0UQA6tLRlqnvuCMPJaQnTVFz7BNCQM8QHPkLALsjnS7YV2_ryjTFIXK7-l-DXYYnXl7IgS6IOiuWmbOsf4WgXNqsZBm5qrLn1B31rgWO3kkxCxJ05Ea85ZKWiUBV-0gpW5JOofjRVokdFF-WOVhMPNrQh_q5KxgOVPdfdcTA6pTWw0DJl4fO1gWWhoBr-Mlm7Y3oH_gCgEZ2Eh4-fENtSm3AUobSRltNiwAwSThzbC7cZkJII_LRyGf6h_4w_tg_w3ew6MjZf_Qkk8H58UXZQEr95FfFzhlSLUH3B58WDHwbi_9SzaDZky120t8Lg6o2Zj3YxnPfKIwOyNYOVaFfMO-6j-Sb1bTU7DWaizpRcC0=s1920-no?authuser=0" alt="" />
           </ImageWrapper>
           <Contents>
             <MyInfo />
