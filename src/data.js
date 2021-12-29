76788dbb102151d5360cb2512219026d2c7b7509
doremichin <johnkim994@naver.com>
2021-12-29 16:08:13 +0900

    [POR-9] blog-typescript 포트폴리오 추가

--- src/data.js
+++ src/data.js
@@ -17,7 +17,7 @@ export const appData = {
         description: [
           'Unsplash public API를 이용해 개발했습니다.',
           '사용자는 원하는 사진을 검색 할 수 있고 색이나 모양별로 필터링을 할 수 있습니다.',
-          '프로젝트가 점점 커지고 컴포넌트의 깊이가 깊어지면서 상태를 전달하기가 번거로워졌고 불필요한 리렌더링이 많이 발생 했습니다. 그래서 redux 사용해 상태관리를 했고 redux-saga를 사용해 api 요청 등 비동기처리를 했습니다.',
+          '프로젝트가 점점 커지고 컴포넌트의 깊이가 깊어지면서 상태를 전달하기가 번거로워졌고 불필요한 리렌더링이 많이 발생 했습니다. 그래서 Redux 사용해 상태관리를 했고 Redux-Saga를 사용해 API 요청 등 비동기처리를 했습니다.',
           '사용자가 스크롤 할 때 지연 없이 새로운 컨텐츠를 불러올 수 있도록 Intersection Obsever를 사용해서 무한 스크롤을 구현 했습니다.',
           '검색한 페이지에서 사진을 필터링 하는 기능은 쿼리스트링으로 필터링 데이터를 전달해서 리렌더링 하는 방식으로 구현 했습니다.',
           '모바일에서도 이용할 수 있도록 반응형 제작을 했습니다.',
@@ -47,7 +47,24 @@ export const appData = {
         jira: '',
         startDate: '/ 2021.11.02~',
         hasLink: true,
-
+      },
+      {
+        name: 'Blog',
+        description: [
+          'Firebase의 Database, Storage, Authentication 서비스를 통해 CRUD와 로그인 로직을 구현 했습니다.',
+          'Typescript를 사용해 컴파일 단계에서 오류를 잡아낼 수 있게 했습니다.',
+          '블로그의 포스팅은 로그인 후 가능하며 수정 및 삭제는 각 포스팅의 uid와 현재 로그인한 유저의 uid가 같을 경우 가능 합니다.',
+          '회원가입도 쉽게 가능 하지만 아래 계정을 사용 하셔도 됩니다.',
+          '아이디 : read@me.com / 비밀번호 : 00000000 (8글자)',
+          'React-Hook-Form을 사용해서 form 태그를 관리 하면서 아이디와 비밀번호의 validation을 체크 했습니다.',
+        ],
+        url: 'https://blog-typescript.netlify.app',
+        imageUrl: 'https://myeoni-portfolio.s3.ap-northeast-2.amazonaws.com/images/blog-typescript+view.png',
+        tags: ['React', 'Typescript', 'Redux', 'Redux-toolkit', 'Styled-Components', 'React-Hook-Form'],
+        github: 'https://github.com/doremichin/blog-typescript',
+        jira: '',
+        startDate: '/ 2021.12.13~',
+        hasLink: true,
       },
       {
         name: 'Etc..',
@@ -56,7 +73,6 @@ export const appData = {
           'Jira의 스크럼 템플릿을 사용해서 명시적으로 이슈를 나누어 개발을 했습니다.',
           'ESLint를 사용해 코드의 문법적 오류를 미리 막고 통일된 스타일로 포맷 했습니다.',
           '현재 이 포트폴리오 사이트는 Next.js를 사용해 제작 했습니다.',
-          'Firebase를 이용해 blog 사이트를 만들어보면서 CRUD 구현을 공부했습니다.',
         ],
         url: '#;',
         imageUrl: 'https://myeoni-portfolio.s3.ap-northeast-2.amazonaws.com/images/youtube-jira.png',
@@ -82,6 +98,10 @@ export const frontSkills = [
   {
     title: 'next.js',
     thumbnails: 'https://blog.kakaocdn.net/dn/cwO2es/btrdDwL8Gum/ceUSfBcd5f7UvBOW7SrW7K/img.png',
+  },
+  {
+    title: 'typescript',
+    thumbnails: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
   },
 ]
 export const softSkills = [
